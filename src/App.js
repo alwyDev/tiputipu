484c3c1f665983164121aea184a8bf2001c824dc
Alwy <wyalwy.dev@gmail.com>
2021-02-19 09:52:10 +0700

    Add Image, Username, and Caption to tiputipu Post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "./Post";
+import Post from "./Post";
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "Patrick",
+      caption: "ngantuk cuy",
+      imageUrl:
+        "https://www.greenscene.co.id/wp-content/uploads/2020/08/Patrick-Stars-696x497.jpg",
+    },
+    {
+      username: "SpongeBob",
+      caption: "Saatnya bekerja!",
+      imageUrl:
+        "https://i.pinimg.com/originals/b4/2a/0b/b42a0bda8428f573054353009edbe61c.jpg",
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app__headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt=""
+        />
+      </div>
+
+      {posts.map((post) => (
+        <Post
+          username={post.username}
+          caption={post.caption}
+          imageUrl={post.imageUrl}
+        />
+      ))}
     </div>
   );
 }
